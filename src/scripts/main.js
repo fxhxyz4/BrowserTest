d9418b8a4c98b62ded92d33be2d044454204f4f1
fxhxyz4 <fxhxyz@proton.me>
2023-03-08 04:43:34 +0200

    -

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,5 @@
 const data = require("../../data/data.json").data;
+const html = document.querySelector("html");
 const target = "_blank";
 
 function test() {
@@ -7,6 +8,8 @@ function test() {
 		window.open(arr, target);
 		console.error(`open: ` + arr);
 	}
+
+	html.style.backgroundColor = "#000";
 }
 
 test();
